87ebb439c6769a239f2705781866d13b955a56d1
Bhargab Dhungel <bhargabdhungel@gmail.com>
2023-10-15 13:02:32 +0530

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
